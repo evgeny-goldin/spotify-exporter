d735edfc623248710321ac031a56dfdcd2365c01
Evgeny Goldin <evgenyg@gmail.com>
2014-10-08 15:27:59 +0200

    Track duration and fetch duration are displayed

--- utils.js
+++ utils.js
@@ -43,7 +43,6 @@ exports.param = function( req, name ){
 }
 
 
-
 /**
  * Exports a playlist specified by sending it in a ZIP response.
  * @param {res}          response to send ZIP to
@@ -60,6 +59,7 @@ exports.export_playlist = function( res, token, user_id, playlist_id ) {
        function( response ){
 
     var playlist = {
+      'start' : new Date().getTime(),
       'name'  : response.name,
       'url'   : response.external_urls.spotify,
       'owner' : response.owner.id,
@@ -71,11 +71,21 @@ exports.export_playlist = function( res, token, user_id, playlist_id ) {
 }
 
 
+/**
+ * Exports a playlist specified by sending it in a ZIP response, paginating over all tracks.
+ * @param {res}        response to send ZIP to
+ * @param {token}      access token
+ * @param {playlist}   playlist JSON
+ * @param {tracks_url} URL to read playlist tracks from, null when pagination is over
+ */
 var export_playlist_paginate = function( res, token, playlist, tracks_url ) {
 
   if ( tracks_url === null ) {
     // Pagination is over, no more tracks to fetch
-    console.log( util.format( "Sending playlist '%s' with %s tracks", playlist.name, playlist.tracks.length ))
+    var fetch_duration = new Date().getTime() - playlist.start;
+    playlist.start     = undefined;
+    console.log( util.format( "%s tracks of playlist '%s' were fetched in %s ms",
+                              playlist.tracks.length, playlist.name, fetch_duration ))
     send_zip_response( res, playlist.name, JSON.stringify( playlist, null, '  ' ));
   } else {
     // Pagination continues, there are more tracks to fetch
@@ -87,6 +97,11 @@ var export_playlist_paginate = function( res, token, playlist, tracks_url ) {
 }
 
 
+/**
+ * Reads playlist tracks.
+ * @param  {track_items} "items" tracks section of Spotify response JSON
+ * @return {array} tracks as array of JSONs, each JSON specifying track's uri, name, etc.
+ */
 var read_tracks = function ( track_items ) {
   // https://developer.spotify.com/web-api/get-playlists-tracks/
   return _.map( track_items, function( item ){
@@ -96,9 +111,26 @@ var read_tracks = function ( track_items ) {
       'preview' : track.preview_url,
       'name'    : track.name,
       'album'   : track.album.name,
-      'artists' : _.map( track.artists, function( artist ){ return artist.name }).join( ', ' )
+      'artists' : _.map( track.artists, function( artist ){ return artist.name }).join( ', ' ),
+      'duration': duration( track.duration_ms )
     }
   })
+}
+
+
+/**
+ * Retrieves track duration as "mm:ss".
+ * @param  {duration_ms}  track duration in milliseconds
+ * @return {string} track duration as "mm:ss"
+ */
+var duration = function( duration_ms ) {
+  var millis_in_second = 1000;
+  var millis_in_minute = millis_in_second * 60;
+  var minutes          = Math.floor( duration_ms / millis_in_minute );
+  var seconds          = Math.ceil(( duration_ms - ( minutes * millis_in_minute )) / millis_in_second );
+  var pad              = function ( n ){ return (( n < 10 ? '0' : '' ) + n )}
+
+  return util.format( "%s:%s", pad( minutes ), pad( seconds ));
 }
 
 
